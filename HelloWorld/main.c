9edd4a4f21f88fc0ee896c4c56878fdb41b1b572
camelvin <celvinacting@gmail.com>
2018-07-12 15:22:30 -0400

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by PHOENIXMAC on 2018-07-12.
+//  Copyright © 2018 PHOENIXMAC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
